dd85bd82b24705f7b8781eb78f7a3533031c09ea
freek <freek@spatie.be>
2017-06-14 20:38:07 +0200

    fix tests

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -9,7 +9,7 @@ class RequestTest extends TestCase
     {
         $response = $this->get('/');
 
-        $this->assertMatchesJsonSnapshot(json_encode($response));
+        $this->assertMatchesSnapshot(json_encode($response));
     }
 
     /** @test */
