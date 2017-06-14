946dedbbb676a1e374f17b502e4f48644ab9a24d
freek <freek@spatie.be>
2017-06-14 23:12:12 +0200

    remove snapshot test

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -5,14 +5,6 @@ namespace Spatie\JsonApiPaginate\Test;
 class RequestTest extends TestCase
 {
     /** @test */
-    public function it_can_return_paginated_results()
-    {
-        $response = $this->get('/');
-
-        $this->assertMatchesJsonSnapshot(json_encode($response));
-    }
-
-    /** @test */
     public function it_will_discover_the_page_size_parameter()
     {
         $response = $this->get('/?page[size]=2');
