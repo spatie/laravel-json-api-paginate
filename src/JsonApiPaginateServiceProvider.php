1916f0a14e3138860c02b842d4e649d5ce32ceb7
Freek Van der Herten <freek@spatie.be>
2017-10-16 13:38:25 +0000

    Apply fixes from StyleCI

--- src/JsonApiPaginateServiceProvider.php
+++ src/JsonApiPaginateServiceProvider.php
@@ -37,7 +37,7 @@ class JsonApiPaginateServiceProvider extends ServiceProvider
             $sizeParameter = config('json-api-paginate.size_parameter');
 
             if (! is_null($maxResults)) {
-            	 $configuredMaximum = $maxResults;
+                $configuredMaximum = $maxResults;
             }
 
             $size = request()->input('page.'.$sizeParameter, $configuredMaximum);
