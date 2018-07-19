800dd8a50efb71488d2aa7a08d1b4184240ec1a6
Ian Nisbet <ian.nisbet@rmpenterprise.co.uk>
2018-07-19 12:46:06 +0100

    ignore maxResults if set to 0, return all if size=-1

--- config/json-api-paginate.php
+++ config/json-api-paginate.php
@@ -6,13 +6,13 @@ return [
      * The maximum number of results that will be returned
      * when using the JSON API paginator.
      */
-    'max_results' => 30,
+    'max_results' => 0,
 
     /*
      * The default number of results that will be returned
      * when using the JSON API paginator.
      */
-    'default_size' => 30,
+    'default_size' => 50,
 
     /*
      * The key of the page[x] query string parameter for page number.
