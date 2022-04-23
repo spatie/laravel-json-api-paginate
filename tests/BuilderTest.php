af13f0d89847ea78fcf2a8357c17de811a9cb8ad
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-04-23 22:38:21 +0300

    Add tests

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -16,6 +16,16 @@ class BuilderTest extends TestCase
     }
 
     /** @test */
+    public function it_can_paginate_records_with_cursor()
+    {
+        config()->set('json-api-paginate.use_cursor_pagination', true);
+
+        $paginator = TestModel::jsonPaginate();
+
+        $this->assertEquals('http://localhost?page%5Bcursor%5D=eyJ0ZXN0X21vZGVscy5pZCI6MzAsIl9wb2ludHNUb05leHRJdGVtcyI6dHJ1ZX0', $paginator->nextPageUrl());
+    }
+
+    /** @test */
     public function it_returns_the_amount_of_records_specified_in_the_config_file()
     {
         config()->set('json-api-paginate.default_size', 10);
@@ -26,8 +36,29 @@ class BuilderTest extends TestCase
     }
 
     /** @test */
+    public function it_returns_the_amount_of_records_specified_in_the_config_file_using_cursor()
+    {
+        config()->set('json-api-paginate.use_cursor_pagination', true);
+        config()->set('json-api-paginate.default_size', 10);
+
+        $paginator = TestModel::jsonPaginate();
+
+        $this->assertCount(10, $paginator);
+    }
+
+    /** @test */
     public function it_can_return_the_specified_amount_of_records()
     {
+        $paginator = TestModel::jsonPaginate(15);
+
+        $this->assertCount(15, $paginator);
+    }
+
+    /** @test */
+    public function it_can_return_the_specified_amount_of_records_with_cursor()
+    {
+        config()->set('json-api-paginate.use_cursor_pagination', true);
+
         $paginator = TestModel::jsonPaginate(15);
 
         $this->assertCount(15, $paginator);
@@ -62,9 +93,28 @@ class BuilderTest extends TestCase
     }
 
     /** @test */
+    public function it_can_use_cursor_pagination()
+    {
+        config()->set('json-api-paginate.use_cursor_pagination', true);
+
+        $paginator = TestModel::jsonPaginate();
+
+        $this->assertFalse(method_exists($paginator, 'total'));
+    }
+
+    /** @test */
     public function it_can_use_base_query_builder()
     {
         $paginator = DB::table('test_models')->jsonPaginate();
         $this->assertEquals('http://localhost?page%5Bnumber%5D=2', $paginator->nextPageUrl());
+    }
+
+    /** @test */
+    public function it_can_use_base_query_builder_with_cursor_pagination()
+    {
+        config()->set('json-api-paginate.use_cursor_pagination', true);
+
+        $paginator = DB::table('test_models')->orderBy('id')->jsonPaginate(10);
+        $this->assertEquals('http://localhost?page%5Bcursor%5D=eyJpZCI6MTAsIl9wb2ludHNUb05leHRJdGVtcyI6dHJ1ZX0', $paginator->nextPageUrl());
     }
 }
--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -21,6 +21,14 @@ class RequestTest extends TestCase
     }
 
     /** @test */
+    public function it_will_discover_the_cursor_parameter()
+    {
+        $response = $this->get('cursor/?page[cursor]=eyJpZCI6MTAsIl9wb2ludHNUb05leHRJdGVtcyI6dHJ1ZX0');
+
+        $response->assertJsonFragment(['prev_cursor' => 'eyJpZCI6MTEsIl9wb2ludHNUb05leHRJdGVtcyI6ZmFsc2V9']);
+    }
+
+    /** @test */
     public function it_will_use_the_default_page_size()
     {
         $response = $this->get('/');
@@ -39,6 +47,16 @@ class RequestTest extends TestCase
     }
 
     /** @test */
+    public function it_will_use_the_configured_page_size_parameter_for_cursor()
+    {
+        config(['json-api-paginate.size_parameter' => 'modified_size']);
+
+        $response = $this->get('cursor/?page[modified_size]=2');
+
+        $response->assertJsonFragment(['per_page' => 2]);
+    }
+
+    /** @test */
     public function it_will_use_the_configured_page_number_parameter()
     {
         config(['json-api-paginate.number_parameter' => 'modified_number']);
@@ -46,5 +64,23 @@ class RequestTest extends TestCase
         $response = $this->get('/?page[modified_number]=2');
 
         $response->assertJsonFragment(['current_page' => 2]);
+    }
+
+    /** @test */
+    public function it_will_use_the_configured_cursor_parameter()
+    {
+        config(['json-api-paginate.cursor_parameter' => 'modified_cursor']);
+
+
+        $response = $this->get('cursor/?page[size]=10&page[modified_cursor]=eyJpZCI6MTAsIl9wb2ludHNUb05leHRJdGVtcyI6dHJ1ZX0');
+        $response->assertJsonFragment(['next_cursor' => 'eyJpZCI6MjAsIl9wb2ludHNUb05leHRJdGVtcyI6dHJ1ZX0']);
+    }
+
+    /** @test */
+    public function it_will_use_the_configured_size_parameter_for_cursor()
+    {
+        $response = $this->get('cursor/?page[size]=10');
+
+        $response->assertJsonFragment(['next_cursor' => 'eyJpZCI6MTAsIl9wb2ludHNUb05leHRJdGVtcyI6dHJ1ZX0']);
     }
 }
